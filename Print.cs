8e1ba7a3b89db6310cc1076667d932fe22dda9cd
komalsonawane12 <99522706+komalsonawane12@users.noreply.github.com>
2022-02-17 08:45:03 +0530

    Create Print.cs
    using System;using System.Collections.Generic;using System.Linq;using System.Text;using System.Threading.Tasks; namespace ConsoleApp1{    class operators     {static void Main(string[] args){int a,b,c;Console.WriteLine("enter 1st no");a=Convert.ToInt32(Console.ReadLine);Console.WriteLine("enter 2nd no");b=Convert.ToInt32(Console.ReadLine);c=a+b;Console.WriteLine("addition of no is"+c);}}    }}

---
+++ Print.cs
@@ -0,0 +1,15 @@
+//Sonawane Komal Nagraj
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ namespace ConsoleApp
+1{    class Program   
+ {static void Main(string[] args)
+{For(int i=0; i<=5; i++)
+{ Console.WriteLine("read one, each one,teach one");
+ Console.ReadLine();}
+}  
+  }
+}
